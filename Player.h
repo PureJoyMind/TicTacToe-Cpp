2d1089689df0ee408506ff54daaecbdea2341f03
Mazdak <mazpar.2000@gmail.com>
2022-03-26 15:14:50 +0430

    Added winCount to Player struct

--- Player.h
+++ Player.h
@@ -8,6 +8,7 @@ struct Player
 	std::string name;// Player name
 	char symbol;// Player symbol X or O
 	char board[3][3];// Player board to check for win
+	unsigned int winCount{ 0 };
 
 	void setName();// Getting player name
 	void setSym();// Getting player symbol
