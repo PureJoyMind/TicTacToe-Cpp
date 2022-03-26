6cc73efaf712ed1a8ff9b7212f4fec991f981140
Mazdak <mazpar.2000@gmail.com>
2022-03-26 15:55:39 +0430

    Added game texts for game loop

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -13,7 +13,43 @@ int main()
     // Initiating game
     GameManager Game;
 
+    cout << "   Welcome to a game of\n" << "    Tic Tac Toe\n\n";
 
+    // Getting players info before starting the game
+    Player p1;
+    cout << "   Player 1:\n";
+    p1.setName();
+    p1.setSym();
+
+    cout << "   Player 2:\n";
+    Player p2;
+    p2.setName();
+    p2.setSym();
     
+    cout << "\n\tThere are four rounds the best of four wins!\n ";
+
+    unsigned int gameCounter{ 1 }; // Counter for game turns
+
+    // X and Y for input
+    unsigned int p1X;
+    unsigned int p1Y;
+    unsigned int p2X;
+    unsigned int p2Y;
+
     Game.boardPrint();
+
+    while (gameCounter <= 4)// Game loop
+    {
+        cout << "\tChoose the coordinates X and Y: \n";
+        cout << "\tPlayer 1\n";
+        cout << "\tThe X axis: ";
+        cin >> p1X;
+        cout << "\tThe Y axis: ";
+        cin >> p1Y;
+        Game.boardAdd(p1X - 1, p1Y - 1, p1.symbol);
+
+        Game.boardPrint();
+
+    }
+    
 }
