06ef70c7a10fcae8ecd51a7f25764e1fc5fc73d3
Mazdak <mazpar.2000@gmail.com>
2022-03-27 11:57:30 +0430

    Changed winCheck function with the one used in tutorial

--- GameManager.cpp
+++ GameManager.cpp
@@ -68,20 +68,30 @@ void GameManager::boardAdd(int x, int y, char sym)
 
 bool GameManager::winCheck(char symbol)
 {
-	for (unsigned int i{ 0 }; i < 3; i++) {
-
-		if (board[i][0] && board[i][1] && board[i][2] == symbol)
-			return true;
-	}
-	for (int j{ 0 }; j < 3; j++) {
-		if (board[0][j] && board[1][j] && board[2][j] == symbol)
-			return true;
+	//Check the rows
+	for (int i = 0; i < 3; i++) {
+		if ((board[i][0] == symbol) && (board[i][0] == board[i][1]) && (board[i][1] == board[i][2])) {
+			return true; //We won!
+		}
 	}
 
-	if (board[0][0] && board[1][1] && board[2][2] == symbol)
-		return true;
+	//Check the columns
+	for (int i = 0; i < 3; i++) {
+		if ((board[0][i] == symbol) && (board[0][i] == board[1][i]) && (board[1][i] == board[2][i])) {
+			return true; //We won!
+		}
+	}
+
+	//Check top left diagonal
+	if ((board[0][0] == symbol) && (board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {
+		return true; //We won!
+	}
+
+	//Check top right diagonal
+	if ((board[2][0] == symbol) && (board[2][0] == board[1][1]) && (board[1][1] == board[0][2])) {
+		return true; //We won!
+	}
 
 	return false;
 }
-
 
